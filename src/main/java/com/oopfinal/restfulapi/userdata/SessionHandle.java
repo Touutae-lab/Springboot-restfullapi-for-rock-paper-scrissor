ff3bcd9cbf052c82a6aad6106278a1b0c00728ca
Saksinkarn Petchkuljinda <=>
2021-09-26 22:00:21 +0700

    Simplify SessionHandle.java

--- src/main/java/com/oopfinal/restfulapi/userdata/SessionHandle.java
+++ src/main/java/com/oopfinal/restfulapi/userdata/SessionHandle.java
@@ -2,22 +2,11 @@ package com.oopfinal.restfulapi.userdata;
 
 public class SessionHandle extends SessionData{
     public boolean checkIfYourself(SessionData session, String id) {
-        String check = session.getPlayer1ID();
-        if (check.equals(id))
-            return true;
-
-        return false;
+        return session.getPlayer1ID().equals(id);
     }
 
     public boolean checkRoom(SessionData session, String id) {
-        String check = session.getPlayer2ID();
-        if (check.equals("")) {
-            session.setPlayer2ID(id);
-            return true;
-        }
-        else {
-            return false;
-        }
+        return session.getPlayer2ID().equals("");
     }
     // SessionHandle(SessionData session, String tmRound) {
         
