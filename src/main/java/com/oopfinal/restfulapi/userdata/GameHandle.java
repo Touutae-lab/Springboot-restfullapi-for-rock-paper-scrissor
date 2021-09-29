841aa7f2a8a145751ade58102d107851d679e55b
Touutae-lab <pantakan.totae@gmail.com>
2021-09-30 05:08:42 +0700

    try this 5

--- src/main/java/com/oopfinal/restfulapi/userdata/GameHandle.java
+++ src/main/java/com/oopfinal/restfulapi/userdata/GameHandle.java
@@ -25,10 +25,10 @@ public class GameHandle {
         Integer Con1 = Data.get(player1.get("Id"));
         Integer Con2 = Data.get(player2.get("Id"));
         if ( Con1 == 0 && Con2 != 0) {
-            Reminder reminder = new Reminder(SesData, 2);
+            Reminder reminder = new Reminder(SesData, 1);
         }
         else if (Con2 == 0 && Con1 != 0) {
-            Reminder reminder = new Reminder(SesData, 1);
+            Reminder reminder = new Reminder(SesData, 2);
         }
     }
 }
